0095f5f394a0715bb93cc7cfab946a463e9ee002
Jean Valentin <jeanvalentin@users.noreply.github.com>
2020-12-21 17:14:11 +0100

    Replace updateQuality in GildedRose

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -19,51 +19,7 @@ final class GildedRose
     public function updateQuality(): void
     {
         foreach ($this->items as $item) {
-            if ($item->name !== 'Aged Brie' and $item->name !== 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    if ($item->name !== 'Sulfuras, Hand of Ragnaros') {
-                        --$item->quality;
-                    }
-                }
-            } else {
-                if ($item->quality < 50) {
-                    ++$item->quality;
-                    if ($item->name === 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sell_in < 11) {
-                            if ($item->quality < 50) {
-                                ++$item->quality;
-                            }
-                        }
-                        if ($item->sell_in < 6) {
-                            if ($item->quality < 50) {
-                                ++$item->quality;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if ($item->name !== 'Sulfuras, Hand of Ragnaros') {
-                --$item->sell_in;
-            }
-
-            if ($item->sell_in < 0) {
-                if ($item->name !== 'Aged Brie') {
-                    if ($item->name !== 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            if ($item->name !== 'Sulfuras, Hand of Ragnaros') {
-                                --$item->quality;
-                            }
-                        }
-                    } else {
-                        $item->quality -= $item->quality;
-                    }
-                } else {
-                    if ($item->quality < 50) {
-                        ++$item->quality;
-                    }
-                }
-            }
+            $item->updateQuality();
         }
     }
 }
